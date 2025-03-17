85b3ce6b25d0d8250ef05fe5ce4d789804b3119c
Matheus <mthsbessac@gmail.com>
2025-03-17 20:41:31 -0300

    create account test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,10 +1,20 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
-func TestCreateAccount(t *testing.T){
+func TestCreateAccount(t *testing.T) {
 	arg := CreateAccountParams{
-		Owner: "John",
+		Owner:   "John",
 		Balance: 1000,
+	}
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	if err != nil {
+		t.Fatal("failed to create account:", err)
+	}
+	if account.Owner != arg.Owner {
+		t.Errorf("expected owner %s, got %s", arg.Owner, account.Owner)
 	}
 }
