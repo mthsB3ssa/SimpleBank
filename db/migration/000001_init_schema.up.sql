e6bd533f6d00481cda46f6216ae7af5748c83bff
Matheus <mthsbessac@gmail.com>
2025-03-18 09:15:32 -0300

    utils and tests

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,21 +1,21 @@
 CREATE TABLE "accounts" (
-  "id" serial PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (now())
-);
-
-CREATE TABLE "transfers" (
-  "id" serial PRIMARY KEY,
-  "from_account_id" integer NOT NULL,
-  "to_account_id" integer NOT NULL,
-  "amount" bigint NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (now())
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "entries" (
-  "id" serial PRIMARY KEY,
-  "account_id" integer NOT NULL,
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -3,18 +3,44 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
+
+	"github.com/mthsB3ssa/SimpleBank/utils"
+	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func CreateRandomAccount(t *testing.T) Account{
 	arg := CreateAccountParams{
-		Owner:   "John",
-		Balance: 1000,
+		Owner:   utils.RandomOwner(),
+		Balance: utils.RandomMoney(),
 	}
+
 	account, err := testQueries.CreateAccount(context.Background(), arg)
-	if err != nil {
-		t.Fatal("failed to create account:", err)
-	}
-	if account.Owner != arg.Owner {
-		t.Errorf("expected owner %s, got %s", arg.Owner, account.Owner)
-	}
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	CreateRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 }
